f6e8eb8b6d120b7db189e023128fa0f4070c7439
bartlomiej-gora <bartlomiej.gora@gmail.com>
2022-11-11 20:54:42 +0100

    init commit

---
+++ src/main/java/com/github/bgora/PersonDTO.java
@@ -0,0 +1,93 @@
+package com.github.bgora;
+
+import java.util.Set;
+
+/**
+ * PersonDTO
+ * Class to send Person data over network
+ * Required fields are PESEL, First Name, Last Name
+ * TODO: Refactor this class to use Builder Pattern
+ */
+public class PersonDTO {
+
+    private final String pesel;
+    private final String firstName;
+    private final String lastName;
+    private String secondName;
+
+    private String thirdName;
+    private Set<PersonDTO> siblings;
+    private Set<PersonDTO> kids;
+
+
+    public PersonDTO(final String pesel, final String firstName, final String lastName) {
+        this.pesel = pesel;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public PersonDTO(final String pesel, final String firstName, final String lastName, final String secondName) {
+        this.pesel = pesel;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.secondName = secondName;
+    }
+
+    public PersonDTO(final String pesel, final String firstName, final String lastName,
+                     final String secondName, final String thirdName) {
+        this.pesel = pesel;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.secondName = secondName;
+        this.thirdName = thirdName;
+    }
+
+    public PersonDTO(final String pesel, final String firstName, final String lastName, final String secondName,
+                     final String thirdName, final Set<PersonDTO> siblings) {
+        this.pesel = pesel;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.secondName = secondName;
+        this.thirdName = thirdName;
+        this.siblings = siblings;
+    }
+
+    public PersonDTO(final String pesel, final String firstName, final String lastName, final String secondName,
+                     final String thirdName, final Set<PersonDTO> siblings, final Set<PersonDTO> kids) {
+        this.pesel = pesel;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.secondName = secondName;
+        this.thirdName = thirdName;
+        this.siblings = siblings;
+        this.kids = kids;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getThirdName() {
+        return thirdName;
+    }
+
+    public Set<PersonDTO> getSiblings() {
+        return siblings;
+    }
+
+    public Set<PersonDTO> getKids() {
+        return kids;
+    }
+}
